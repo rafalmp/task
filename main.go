297710b377ecc3ad3fbdb332bf155bb5262c27da
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-28 00:50:57 +0200

    Create tasks bucket

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/rafalmp/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/rafalmp/task/cmd"
+	"github.com/rafalmp/task/db"
+)
 
 func main() {
+	home, err := homedir.Dir()
+	checkErr(err)
+	dbPath := filepath.Join(home, "tasks.db")
+	checkErr(db.Init(dbPath))
 	cmd.Execute()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
