67b5664b9243b5f9af63376e9231728a92a286fb
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-05-04 23:03:49 +0200

    Implement list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/rafalmp/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +16,18 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all incomplete tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("This is a fake \"list\" command.")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("An error occurred: ", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("Nothing to do, go get some rest!")
+			return
+		}
+		fmt.Println("Your task list:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s", i+1, task.Value)
+		}
 	},
 }
