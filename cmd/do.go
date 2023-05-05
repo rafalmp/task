7383728ceaaa43ac23923ebc1fe422dd2513b136
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-05-06 01:06:36 +0200

    Implement do command

--- cmd/do.go
+++ cmd/do.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/rafalmp/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -25,6 +26,23 @@ var doCmd = &cobra.Command{
 				taskIds = append(taskIds, id)
 			}
 		}
-		fmt.Printf("Marking task(s) as complete: %v\n", taskIds)
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("An error occurred: ", err)
+			return
+		}
+		for _, id := range taskIds {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed\n", id)
+			}
+		}
 	},
 }
